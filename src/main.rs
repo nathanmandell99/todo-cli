81e162bcc005c2f8998a64039d895ae46ed01d04
nathanmandell99 <nathanmandell99@gmail.com>
2025-06-25 09:44:29 -0400

    Refactor to support error propagation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::error::Error;
 use std::fs::{File, OpenOptions};
 use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::{env, fs};
@@ -26,8 +27,14 @@ fn read_tasks(
     incomplete_tasks: &mut Vec<Task>,
     complete_tasks: &mut Vec<Task>,
     file_name: &String,
-) {
-    let f: File = File::open(file_name).expect("Error: No such file");
+) -> Result<(), Box<dyn Error>> {
+    let f: File = match File::open(file_name) {
+        Ok(file) => file,
+        Err(err) => {
+            eprintln!("Failed to open file: {err:?}");
+            std::process::exit(1);
+        }
+    };
     let reader: BufReader<File> = BufReader::new(f);
 
     let mut line: String;
@@ -38,18 +45,17 @@ fn read_tasks(
     let mut cur_task: Task;
 
     for line_result in reader.lines() {
-        line = line_result.expect("Failed to read line");
+        line = line_result?;
         fields = line.split(",").collect();
         id = fields[0];
         if id == "id" || id == "MAX" {
-            // skip first two lines of the csv
             continue;
         }
         title = fields[1];
-        done = if fields[2] == "true" { true } else { false };
+        done = fields[2] == "true";
 
         cur_task = Task {
-            id: id.parse().unwrap(),
+            id: id.parse()?,
             title: String::from(title),
             done,
         };
@@ -59,41 +65,41 @@ fn read_tasks(
             incomplete_tasks.push(cur_task);
         }
     }
+    Ok(())
 }
 
-fn get_max_task_id(file_name: &String) -> u32 {
-    let f: File = File::open(file_name).expect("Error: No such file");
+fn get_max_task_id(file_name: &String) -> Result<u32, Box<dyn Error>> {
+    let f: File = File::open(file_name)?;
     let mut reader: BufReader<File> = BufReader::new(f);
     let mut line = String::new();
-    reader
-        .read_line(&mut line)
-        .expect("Error: Could not read line");
+    reader.read_line(&mut line)?;
     let fields: Vec<&str> = line.split(",").collect();
     if fields[0] != "MAX" {
-        return 0;
+        return Ok(0);
     }
-    fields[1].trim().parse().unwrap()
+    Ok(fields[1].trim().parse()?)
 }
 
-fn set_max_task_id(file_name: &String, new_max: u32) {
-    let list_contents = fs::read_to_string(file_name).expect("Failed to read file");
+fn set_max_task_id(file_name: &String, new_max: u32) -> Result<(), Box<dyn Error>> {
+    let list_contents = fs::read_to_string(file_name)?;
     let mut lines: Vec<&str> = list_contents.lines().collect();
 
     let header_format = format!("MAX,{new_max}");
     let new_header: &str = &header_format;
     lines[0] = new_header;
 
-    fs::write(file_name, lines.join("\n")).expect("Failed to update max task id");
+    fs::write(file_name, lines.join("\n"))?;
+    Ok(())
 }
 
 // fn read_task_from_line(task: Task, line: &String) {}
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 3 {
         print_usage();
-        return;
+        ()
     }
 
     let command: &str = &args[2];
@@ -104,31 +110,29 @@ fn main() {
             let f: File = OpenOptions::new()
                 .write(true)
                 .append(true)
-                .open(file_name)
-                .expect("Failed to open {file_name}");
+                .open(file_name)?;
 
             let mut writer: BufWriter<File> = BufWriter::new(f);
             let title = match args.get(3) {
                 Some(t) => t,
                 None => {
                     print_usage();
-                    return;
+                    return Ok(());
                 }
             };
 
-            let id = get_max_task_id(file_name) + 1;
+            let id = get_max_task_id(file_name)? + 1;
             let buf = String::from(format!("\n{id},{title},false"));
-            writer
-                .write_all(buf.as_bytes())
-                .expect("Failed to write to file");
+            writer.write_all(buf.as_bytes())?;
 
-            set_max_task_id(file_name, id);
+            set_max_task_id(file_name, id)?;
             println!("New task created with title {title}");
+            Ok(())
         }
         "list" => {
             let mut complete_tasks: Vec<Task> = Vec::new();
             let mut incomplete_tasks: Vec<Task> = Vec::new();
-            read_tasks(&mut incomplete_tasks, &mut complete_tasks, &file_name);
+            read_tasks(&mut incomplete_tasks, &mut complete_tasks, &file_name)?;
 
             println!("To-do:");
             for task in incomplete_tasks {
@@ -139,13 +143,14 @@ fn main() {
             for task in complete_tasks {
                 task.print_self();
             }
+            Ok(())
         }
         "done" => {
             let task_id = match args.get(3) {
                 Some(t) => t,
                 None => {
                     print_usage();
-                    return;
+                    return Ok(());
                 }
             };
             let list_contents = fs::read_to_string(file_name).expect("Failed to read file");
@@ -163,24 +168,27 @@ fn main() {
                     let new_line = fields.join(",");
                     lines[i] = &new_line;
                     fs::write(file_name, lines.join("\n")).expect("Failed to mark task done");
-                    return;
+                    return Ok(());
                 }
             }
             println!("Task not found. To see valid tasks, try: ./todo {file_name} list");
+            Ok(())
         }
         "init" => {
             println!("file_name: {file_name}");
             let mut f: File = OpenOptions::new()
                 .write(true)
                 .create(true)
-                .open(file_name)
-                .expect("Failed to create new tasklist");
+                .open(file_name)?;
 
             let init_contents = String::from("MAX,0\nid,description,completed");
-            f.write(init_contents.as_bytes())
-                .expect("Failed to write initial contents");
+            f.write(init_contents.as_bytes())?;
             println!("Initialized new tasklist {file_name}.");
+            Ok(())
         }
-        _ => print_usage(),
+        _ => {
+            print_usage();
+            Ok(())
+        }
     }
 }
